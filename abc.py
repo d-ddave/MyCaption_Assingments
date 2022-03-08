47bbaafc7a8c7251b7db7a238a7be0f9898e225e
d-ddave <93904278+d-ddave@users.noreply.github.com>
2022-03-08 10:38:45 +0530

    Add files via upload

---
+++ abc.py
@@ -0,0 +1,5 @@
+#program to print radius oof a circle for my_captian assignments
+
+from math import pi
+r = float(input ("Input the radius of the circle : "))
+print ("The area of the circle with radius " + str(r) + " is: " + str(pi * r**2))
